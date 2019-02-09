885ae2fedb3278cb1ac4193149f4744ee7c3f353
sean-one <basement365@gmail.com>
2019-02-09 12:39:20 -0800

    working during weekend

--- python/traversal.py
+++ python/traversal.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import math
 from variables import token, tokenContent
 from stack import Stack
 
@@ -45,10 +46,11 @@ def findExits(exits):
     else:
         return None
 
-roomInfo = viewRoom()
+
+roomInfo = requests.get(f'{urlRoot}init/', headers=token).json()
 time.sleep(roomInfo['cooldown'])
 
-mapGraph = {0: [(60, 60), {'n': '?', 's': '?', 'w': '?', 'e': '?'}]}
+mapGraph = {0: {'n': '?', 's': '?', 'w': '?', 'e': '?', 'c': '?'}}
 inversalKey = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
 stroll = []
 
@@ -56,40 +58,45 @@ exploreStack = Stack()
 exploreStack.push(roomInfo['room_id'])
 
 print(exploreStack.stack)
-
 while exploreStack.size() < 5:
-    exitOptions = mapGraph[roomInfo['room_id']][1]
+    print(f'before {mapGraph}')
+    exitOptions = mapGraph[exploreStack.stack[-1]]
+    print(f'after {mapGraph}')
     checkExits = findExits(exitOptions)
     if checkExits is not None:
         firstExit = checkExits[0]
         enterFrom = exploreStack.stack[-1]
         stroll.append(firstExit)
-        newRoom = blindExplore(firstExit)
+        newRoom = requests.post(f'{urlRoot}move/', headers=tokenContent, json={ 'direction' : firstExit}).json()
+        time.sleep(newRoom['cooldown'])
         print(newRoom)
-        time.sleep(roomInfo['cooldown'])
 
-        mapGraph[enterFrom][1][firstExit] = roomInfo['room_id']
+        mapGraph[enterFrom][firstExit] = roomInfo['room_id']
 
         exitDict = {}
         if newRoom['room_id'] in exploreStack.stack:
-            mapGraph[newRoom['room_id']][1][inversalKey[firstExit]] = enterFrom
+            mapGraph[newRoom['room_id']][inversalKey[firstExit]] = enterFrom
         else:
             for exit in newRoom['exits']:
-                exitDict[exit] = '?'
+                if exit == 'c':
+                    exitDict[exit] = newRoom['coordinates']
+                else:
+                    exitDict[exit] = '?'
             exitDict[inversalKey[firstExit]] = enterFrom
-            mapGraph[newRoom['room_id']][0] = newRoom['coordinates']
-            mapGraph[newRoom['room_id']][1] = exitDict
+            mapGraph[newRoom['room_id']] = exitDict
         exploreStack.push(newRoom['room_id'])
+        print(exploreStack.stack)
     
     else:
         finishedRoom = exploreStack.pop()
         if exploreStack.size() > 0:
             lastRoom = exploreStack.stack[-1]
-            for route, room in mapGraph[lastRoom][1].items():
+            for route, room in mapGraph[lastRoom].items():
                 if room == finishedRoom:
                     stroll.append(inversalKey[route])
-                    roomInfo = wiseExplore(room, inversalKey[route])
+                    roomInfo = requests.post(f'{urlRoot}move/', headers=tokenContent, json={'direction': inversalKey[route]}).json()
                     time.sleep(roomInfo['cooldown'])
+                    # roomInfo = blindExplore(inversalKey[route])
                     break
 print(mapGraph)
 
