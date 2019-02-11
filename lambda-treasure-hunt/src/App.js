74d7e59316aac87af415c4decff478032718bd69
SeanOne <basement365@gmail.com>
2019-02-11 13:51:33 -0800

    adding components

--- lambda-treasure-hunt/src/App.js
+++ lambda-treasure-hunt/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import locations from '../src/python/locations.json'
+
+// Components
+import Movement from './Movement/Movement';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      mapGraph: locations,
-    }
   }
   render() {
-    console.log(locations)
     return (
       <div className="App">
-        <h1>THIS IS THE HEADER</h1>
+        <h1>Lambda Treasure Hunt</h1>
+        <Movement />
       </div>
     );
   }
---
+++ lambda-treasure-hunt/src/Movement/Movement.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Movement = (props) => {
+    return (
+        <div>
+            <span>Move Up</span>
+            <span>Move Left</span>
+            <span>Move Right</span>
+            <span>Move Down</span>
+        </div>
+    )
+}
+
+export default Movement;
